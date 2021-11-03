db54c25f1d100447364d758adc7ca234e54df348
Huaiqing <Huaiqing@gmail.com>
2021-11-03 14:46:00 +1100

    bs start

---
+++ Gerald and Rose/gerald-and-rose-web/src/components/pages/businessService.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function businessService() {
+    return (
+        <div className='bs-container'>
+            <div className='bs-banner-container'>
+                <div className='bs-banner-title'>
+                </div>
+                <div className='bs-banner-subtitle'>
+                </div>
+                <div className='bs-banner-logo'>
+                </div>
+            </div>
+            <div className='service-container'>
+                <div className='service-container-marketing'>
+                    <div className='service-heading'>
+                    </div>
+                    <div className='service-img'>
+                    </div>
+                    <div className='service-description'>
+                    </div>
+                </div>
+                <div className='service-container-consulte'>
+                <div className='service-heading'>
+                    </div>
+                    <div className='service-img'>
+                    </div>
+                    <div className='service-description'>
+                    </div>
+                </div>
+                <div className='service-container-graphDesign'>
+                <div className='service-heading'>
+                    </div>
+                    <div className='service-img'>
+                    </div>
+                    <div className='service-description'>
+                    </div>
+                </div>
+                <div className='service-container-campaign'>
+                <div className='service-heading'>
+                    </div>
+                    <div className='service-img'>
+                    </div>
+                    <div className='service-description'>
+                    </div>
+                </div>                
+            </div>            
+        </div>
+    )
+}
+
+export default businessService
