84a6f7785ffb8c518cb42bcbfeec7772932277e8
Huaiqing <Huaiqing@gmail.com>
2021-11-03 16:52:14 +1100

    bs msg

--- Gerald and Rose/gerald-and-rose-web/src/components/pages/businessService.js
+++ Gerald and Rose/gerald-and-rose-web/src/components/pages/businessService.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-function Services({bsTitle, bsSubtitle, bsBanner, bsBanneralt}) {
+function Services({
+    bsTitle, bsSubtitle,bsBanner, bsBanneralt, 
+    mkt_img,mkt_alt, mkt_description,
+    con_img,con_alt,con_description,
+    gd_img,gd_alt, gd_description,
+    com_img,com_alt,com_description}) 
+    {
     return (
         <div className='bs-container'>
             <div className='bs-banner-container'>
@@ -17,34 +23,46 @@ function Services({bsTitle, bsSubtitle, bsBanner, bsBanneralt}) {
             <div className='service-container'>
                 <div className='service-container-marketing'>
                     <div className='service-heading'>
+                        <h2>Marketing Advisory</h2>
                     </div>
                     <div className='service-img'>
+                    <img src={mkt_img} alt={mkt_alt} />
                     </div>
                     <div className='service-description'>
+                    <p>{mkt_description}</p>
                     </div>
                 </div>
                 <div className='service-container-consulte'>
                 <div className='service-heading'>
+                <h2>Business Consultancy</h2>
                     </div>
                     <div className='service-img'>
+                        <img src={con_img} alt={con_alt} />
                     </div>
                     <div className='service-description'>
+                    <p>{con_description}</p>
                     </div>
                 </div>
                 <div className='service-container-graphDesign'>
                 <div className='service-heading'>
+                <h2>Graph Design</h2>
                     </div>
                     <div className='service-img'>
+                    <img src={gd_img} alt={gd_alt} />
                     </div>
                     <div className='service-description'>
+                    <p>{gd_description}</p>
                     </div>
                 </div>
                 <div className='service-container-campaign'>
                 <div className='service-heading'>
+                <h2>Compaign Management</h2>
                     </div>
                     <div className='service-img'>
+                    <img src={com_img} alt={com_alt} />
                     </div>
                     <div className='service-description'>
+                        <p>{com_description}</p>
                     </div>
                 </div>                
             </div>            
--- Gerald and Rose/gerald-and-rose-web/src/components/pages/businessService/Data.js
+++ Gerald and Rose/gerald-and-rose-web/src/components/pages/businessService/Data.js
@@ -3,5 +3,17 @@ export const bsObjOne = {
     bsSubtitle: 'A wide range of professional services customized to meet your specific needs.', 
     bsBanner:'', 
     bsBanneralt:'',
+    mkt_img:'',
+    mkt_alt:'', 
+    mkt_description:'We work with businesses to create and implement marketing strategies. We help monitor results and tweak campaigns as they go to ensure that companies get the best results from their marketing efforts. This can include established businesses or ones not yet created.',
+    con_img:'',
+    con_alt:'',
+    con_description:"Many of our clients trust us to help them with their business needs. We have a wide range of in-house and external skills to help clients reach their business growth needs. This might include the provision of bookkeeping, facilitation or business planning services.",
+    gd_img:'',
+    gd_alt:'', 
+    gd_description:"Graphic design is the art or skill of combining text and pictures in advertisements, magazines, or books. Yes it's about the right picture and photos, the right text, all in the right place. We can provide a wide range of services to get your communications looking right. ",
+    com_img:'',
+    com_alt:'',
+    com_description:'Getting the right campaign, targeted at the right audience ensures the digital marketing effort leads to success. We work with you to identify your goals, your audience and the prepare a plan that will see you as a winner.'
 }
 
