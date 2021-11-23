7287c1408be9bac9ae6c95ffb649a2d0ed73ebda
Huaiqing <Huaiqing@gmail.com>
2021-11-23 14:19:36 +1100

    fix

--- Gerald and Rose/gerald-and-rose-web/src/components/pages/post.js
+++ Gerald and Rose/gerald-and-rose-web/src/components/pages/post.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import './post.css'
 
 function Blogpost({post_description, please_note}) {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function loadPosts() {
+            const response = await fetch('/wp-json/wp/v2/posts');
+            if(!response.ok) {
+                // oups! something went wrong
+                return;
+            }
+    
+            const posts = await response.json();
+            setPosts(posts);
+        }
+    
+        loadPosts();
+   }, [])
     return (
         <>
         <div className='post-container'>
@@ -11,7 +30,25 @@ function Blogpost({post_description, please_note}) {
             <div className='please-note'>{please_note}</div>
             </div>
             <div className='post_list'>
-
+              <Grid container spacing={2}>
+      {posts.map((post, index) => (
+      <Grid item xs={4} key={index}>
+        <Card>
+           <CardContent>
+                <Typography
+                    color="textSecondary"
+                    gutterBottom
+                    dangerouslySetInnerHTML={{__html: post.title.rendered}} />
+                <Typography
+                    variant="body2"
+                    component="p"
+                    dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+            </CardContent>
+        </Card>
+      </Grid>
+     ))}
+              </Grid>
+ );
             </div>
         </div>
         </>
