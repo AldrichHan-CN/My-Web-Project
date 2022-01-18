a6d46c910c10ab75a2f7ab67d947f10eebb7e263
Huaiqing <hhan0@utas.edu.au>
2022-01-18 23:55:19 +1100

    18.1.21

--- Personal Website/js/korobeiniki.js
+++ Personal Website/js/korobeiniki.js
@@ -1,4 +1,4 @@
-/* Meun -- Show bottom menu hidden */
+/*----------------- Meun -- Show bottom menu hidden ------------------------*/
 
 const navMenu = document.getElementById('nav-menu'),
       
@@ -31,3 +31,48 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*----------------------------Skill Section--Skill Card arrow actions------------------ */
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i = 0; i < skillsContent.length; i++){
+
+        skillsContent[i].className = 'skills__content skills__close'
+
+    }
+
+    if(itemClass === 'skills__content skills__close'){
+
+        this.parentNode.className = 'skills__content skills__open'
+
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/*--------------------------qualification Tab ----------------------------- */
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+    tabs.forEach(tab => {
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
